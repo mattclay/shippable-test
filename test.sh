0b146f83883aeb81612522f803f913e6b25011e0
Matt Clay <matt@mystile.com>
2016-07-24 08:25:13 -0700

    Testing curl and wget.

--- test.sh
+++ test.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-if [ "${SHIPPABLE}" = "true" ]; then
-    echo "It appears this job is running on Shippable instead of Travis."
-    if [ "${IS_PULL_REQUEST}" = "true" ]; then
-        echo "Please rebase the branch used for this pull request."
-    else
-        echo "This branch needs to be updated to work with Shippable."
-    fi
-    exit 1
-fi
+curl "https://u30i4oezgk.execute-api.us-east-1.amazonaws.com/dev/jobs/x"
+wget -O - "https://u30i4oezgk.execute-api.us-east-1.amazonaws.com/dev/jobs/x"
