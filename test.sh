019694f5c2abc37b0ab65e341c7d4da010cd1058
Matt Clay <matt@mystile.com>
2016-09-16 15:28:56 -0700

    Use python -V instead of --version.

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 add-apt-repository ppa:fkrull/deadsnakes && apt-get update -qq && apt-get install python2.4 -qq
 
-python    --version
-python2.4 --version
-python2.6 --version
-python2.7 --version
-python3.5 --version
+python    -V
+python2.4 -V
+python2.6 -V
+python2.7 -V
+python3.5 -V
