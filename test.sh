247acdc50c9bd1c24847a904067f251d1b220bbf
Matt Clay <matt@mystile.com>
2016-11-15 20:26:52 -0800

    Report locations and versions.

--- test.sh
+++ test.sh
@@ -1,9 +1,26 @@
-#!/bin/bash
+#!/bin/bash -eux
 
-add-apt-repository ppa:fkrull/deadsnakes && apt-get update -qq && apt-get install python2.4 -qq
+add-apt-repository ppa:fkrull/deadsnakes
+apt-get update -qq
+apt-get install python2.4 shellcheck -qq
 
+which python
 python    -V
+
+which python2.4
 python2.4 -V
+
+which python2.6
 python2.6 -V
+
+which python2.7
 python2.7 -V
+
+which python3.5
 python3.5 -V
+
+which coverage
+coverage --version
+
+which shellcheck
+shellcheck --version
