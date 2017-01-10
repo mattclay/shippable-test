4ea96837a0a729fc6172b162c195998edce8845d
Matt Clay <matt@mystile.com>
2017-01-10 11:39:50 -0800

    Check for python 3.6.

--- test.sh
+++ test.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -eux
 
-apt-add-repository 'deb http://archive.ubuntu.com/ubuntu trusty-backports universe'
-apt-get update -qq
-apt-get install shellcheck
-shellcheck test.sh
+python3.6 -V
