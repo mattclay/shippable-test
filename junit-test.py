3c0784fa7a33f968aab380f155e9fa20c8d9e3f7
Matt Clay <matt@mystile.com>
2017-02-16 21:11:12 -0800

    More junit tests.

--- junit-test.py
+++ junit-test.py
@@ -7,17 +7,27 @@ from junit_xml import TestSuite, TestCase
 test_cases = [
     # passing tests (those without error_info, failure_info, skipped_info) are counted but show no output in Shippable UI
     TestCase(name='test-case-name', classname='classname', elapsed_sec=1.2, stdout='stdout', stderr='stderr'),
+
     # error tests show error message and error output in Shippable UI (but not stdout/stderr)
     TestCase(name='test-case-name-error', classname='classname', elapsed_sec=1.2, stdout='stdout', stderr='stderr'),
     # failure tests show failure message and failure output in Shippable UI (but not stdout/stderr)
     TestCase(name='test-case-name-failure', classname='classname', elapsed_sec=1.2, stdout='stdout', stderr='stderr'),
     # skipped tests are counted but show no output in Shippable UI
     TestCase(name='test-case-name-skipped', classname='classname', elapsed_sec=1.2, stdout='stdout', stderr='stderr'),
+
+    # more test cases
+    TestCase(name='test-case-name-error-no-output', classname='classname', elapsed_sec=1.2, stdout='stdout', stderr='stderr'),
+    TestCase(name='test-case-name-failure-no-output', classname='classname', elapsed_sec=1.2, stdout='stdout', stderr='stderr'),
+    TestCase(name='test-case-name-skipped-no-output', classname='classname', elapsed_sec=1.2, stdout='stdout', stderr='stderr'),
 ]
 
 test_cases[1].add_error_info(message='error message', output='error output')
 test_cases[2].add_failure_info(message='failure message', output='failure output')
 test_cases[3].add_skipped_info(message='skipped message', output='skipped output')
+
+test_cases[4].add_error_info(message='error message')
+test_cases[5].add_failure_info(message='failure message')
+test_cases[6].add_skipped_info(message='skipped message')
 
 test_suites = [
     TestSuite(
