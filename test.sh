e6b2ff4a2c238b352142aea031cabcedab66fa03
Matt Clay <matt@mystile.com>
2017-03-15 17:02:56 -0700

    Python version check.

--- test.sh
+++ test.sh
@@ -1,3 +1,21 @@
-#!/bin/bash -eux
+#!/bin/bash
+
+which python2
+python2 -V
+
+which python2.6
+python2.6 -V
+
+which python2.7
+python2.7 -V
+
+which python3
+python3 -V
+
+which python3.5
+python3.5 -V
+
+which python3.6
+python3.6 -V
 
 env
