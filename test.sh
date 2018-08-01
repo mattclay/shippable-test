781c1ce4f0652c3215e693a83d97201cff8f928c
Matt Clay <matt@mystile.com>
2018-08-01 16:52:22 -0700

    Install cryptography and requests.

--- test.sh
+++ test.sh
@@ -1,14 +1,4 @@
 #!/bin/bash -x
 
-env
-
-curl https://bootstrap.pypa.io/get-pip.py | python3.6
-
-which pip
-which pip3.6
-
-pip list
-pip3.6 list
-
-python -m pip list
-python3.6 -m pip list
+curl --silent https://bootstrap.pypa.io/get-pip.py | python3.6
+pip3.6 install cryptography requests
