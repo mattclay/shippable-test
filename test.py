6894527c54faa46a1e5dbffb6a32a47550acaafe
Matt Clay <matt@mystile.com>
2018-08-06 21:02:18 -0700

    Test with openssl instead of cryptography.

--- test.py
+++ test.py
@@ -9,9 +9,10 @@ import datetime
 import os
 import requests
 import time
+import subprocess
 
 
-def sign(data_string):
+def sign_cryptography(data_string):
     private_key = ec.generate_private_key(ec.SECP384R1(), default_backend())
     public_key = private_key.public_key()
 
@@ -32,6 +33,29 @@ def sign(data_string):
     return serialized_public_string, signature_string
 
 
+def sign_openssl(data_string):
+    with open('data.txt', 'w') as fd:
+        fd.write(data_string)
+
+    subprocess.check_output(['openssl', 'ecparam', '-name', 'secp384r1', '-genkey', '-noout', '-out', 'private.der', '-outform', 'der'])
+    subprocess.check_output(['openssl', 'ec', '-in', 'private.der', '-inform', 'der', '-pubout', '-out', 'public.der', '-outform', 'der'])
+    subprocess.check_output(['openssl', 'dgst', '-sha256', '-sign', 'private.der', '-keyform', 'der', '-out', 'signature.out', 'data.txt'])
+
+    with open('private.der', 'rb') as fd:
+        serialized_public = fd.read()
+
+    with open('signature.out', 'rb') as fd:
+        signature = fd.read()
+
+    b64_key = base64.b64encode(serialized_public).decode('utf-8')
+
+    serialized_public_string = f'-----BEGIN PUBLIC KEY----- {b64_key} -----END PUBLIC KEY-----'
+
+    signature_string = base64.b64encode(signature).decode('utf-8')
+
+    return serialized_public_string, signature_string
+
+
 def output(message):
     print(message, flush=True)
 
@@ -42,7 +66,8 @@ def main():
 
     output(f'job_id: {job_id}')
 
-    public_key, signature = sign(job_id)
+    # public_key, signature = sign_cryptography(job_id)
+    public_key, signature = sign_openssl(job_id)
 
     output(public_key)
 
--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -x
 
-openssl ecparam -name secp384r1 -genkey -noout
-
 curl --silent https://bootstrap.pypa.io/get-pip.py | python3.6
-pip3.6 install cryptography requests
+pip3.6 install requests
 ./test.py
