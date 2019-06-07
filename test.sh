777bdef4d3bc2427e31831382fd37e3f31f54f6f
Matt Clay <matt@mystile.com>
2019-06-07 10:48:39 -0700

    Show more pip details.

--- test.sh
+++ test.sh
@@ -8,8 +8,12 @@ python --version
 command -V pip
 pip --version
 
+python -m pip --version
+
 command -V python3.7
 python3.7 --version
 
 command -V pip3.7
 pip3.7 --version
+
+python3.7 -m pip --version
