79a3b693cc6d1415f9b2ffea54abcce712dc9190
Matt Clay <matt@mystile.com>
2019-06-07 12:41:29 -0700

    Test 3.7 only.

--- test.sh
+++ test.sh
@@ -2,26 +2,8 @@
 
 set -ux
 
-command -V python
-python --version
-
-command -V pip
-pip --version
-
-python -m pip --version
-
-command -V python3.6
-python3.6 --version
-
-command -V pip3.6
-pip3.6 --version
-
-python3.6 -m pip --version
-
-command -V python3.7
+python3.7 -m ensurepip
 python3.7 --version
-
-command -V pip3.7
 pip3.7 --version
-
-python3.7 -m pip --version
+command -V python3.7
+command -V pip3.7
