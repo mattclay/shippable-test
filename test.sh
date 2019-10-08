2b752a0746af55604d48b1eff76c7e4c6bcaec4e
Matt Clay <matt@mystile.com>
2019-10-08 09:38:59 -0700

    Show openssl version.

--- test.sh
+++ test.sh
@@ -2,14 +2,4 @@
 
 set -ux
 
-python -m pip --version
-python -m pip list
-
-python2.7 -m pip --version
-python2.7 -m pip list
-
-python3.5 -m pip --version
-python3.5 -m pip list
-
-python3.7 -m pip --version
-python3.7 -m pip list
+openssl version
